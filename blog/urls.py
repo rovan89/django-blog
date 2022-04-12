7130d76c45469df5ba40c004955b985b044a4f46
rovan89 <devireland89@gmail.com>
2022-04-12 15:00:31 +0000

    Searching for bug

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
 from . import views
-
+from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home"),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
